05ed7406a3b5a65a1693d8c583dd3adbddd4c4b1
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-06 01:15:08 -0300

    Estrutura criada

---
+++ js/estrutura/estrutura.js
@@ -0,0 +1,16 @@
+// instrução
+console.log("quebre 3 ovos")
+console.log("misture com a farinha")
+console.log("leve ao forno")
+
+// Blocos de códigos
+{
+  console.log("coma 1 pedaço de pizza")
+}
+
+{
+  console.log("teste")
+  {
+    console.log("teste")
+  }
+}
