067b92e573ff1b8d0898f61c6b1b16334efefea6
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-10 00:57:40 -0300

    Aprendendo operadores relacionais

---
+++ js/operadores e funcoes/operadoresRelacionais.js
@@ -0,0 +1,21 @@
+/* Operadores relacionais (retornar verdadeiro ou falso)
+
+== (igual a)
+!=  (diferente)
+> (maior que)
+< (menor que)
+>= (Maior ou igual)
+<= (Menor ou igual)
+*/
+
+let n1 = 20
+let n2 = 10
+// let resultado = n1 <= n2
+
+let idade = 25
+//let resultado = idade > 18
+
+let pedidoMinimo = 20
+let compraPessoa = 100
+let resultado = compraPessoa > pedidoMinimo
+console.log(resultado)
