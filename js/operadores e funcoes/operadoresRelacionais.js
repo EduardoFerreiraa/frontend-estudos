f092a757717ad4e40fea5f628e135350a2076982
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-10 01:05:32 -0300

    Utilizando operadores lógicos

--- js/operadores e funcoes/operadoresRelacionais.js
+++ js/operadores e funcoes/operadoresRelacionais.js
@@ -1,5 +1,4 @@
-/* Operadores relacionais (retornar verdadeiro ou falso)
-
+/* Operadores relacionais (retornar verdadeiro ou falso$)
 == (igual a)
 !=  (diferente)
 > (maior que)
@@ -8,14 +7,28 @@
 <= (Menor ou igual)
 */
 
-let n1 = 20
-let n2 = 10
+//let n1 = 20
+//let n2 = 10
 // let resultado = n1 <= n2
 
-let idade = 25
+//let idade = 25
 //let resultado = idade > 18
 
-let pedidoMinimo = 20
-let compraPessoa = 100
-let resultado = compraPessoa > pedidoMinimo
+//let pedidoMinimo = 20
+//let compraPessoa = 100
+//let resultado = compraPessoa > pedidoMinimo
+//console.log(resultado)
+
+
+
+
+/* Operadores lógicos (testa condições com true ou false)
+&& (e)
+|| (ou)
+*/
+
+let idade = 45
+//let resultado = 2==2 || 2==5
+let resultado = idade >= 18 && idade <= 40
 console.log(resultado)
+
