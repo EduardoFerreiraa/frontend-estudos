013ae35a36613737d09ab413d007736822b41a1e
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-10 00:31:04 -0300

    Aprendendo precedência

--- js/operadores e funcoes/operadoresBasicos.js
+++ js/operadores e funcoes/operadoresBasicos.js
@@ -13,7 +13,14 @@ console.log(resultado)
 
 /*
 Precedência de execução
+0) Parênteses
 1) Multiplicação e divisão
+2) Adição e subtração
 */
 
-4 + 4 / 2
+// 4 + 4 / 2
+2 + 2 / 2 * 3
+2 + 2 * 2 / 3
+2 * 2 / 2
+5 - 3 / 2
+5 + 5 - 8
