b4029c6254dcfa12642ae4b4e70ebf2fc85ae25d
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-19 00:19:01 -0300

    Usando parametros para calcular a media de notas

--- js/operadores e funcoes/funcoes.js
+++ js/operadores e funcoes/funcoes.js
@@ -4,11 +4,16 @@
 3) Ir ao mercado comprar arroz;
 */
 
-function irMercadoComprarArroz(dinheiro) {
-    return "arroz"
+function calcularMedia(nota1, nota2) {
+    
+    let totalNotas = nota1 + nota2
+    let media = totalNotas / 2
+
+    return media
 }
 
 // desligarTorneira()
-let retorno = irMercadoComprarArroz(10)
-console.log(retorno)
-
+// let retorno = irMercadoComprarArroz(10)
+// console.log(retorno)
+let media = calcularMedia(10, 5)
+console.log(media)
