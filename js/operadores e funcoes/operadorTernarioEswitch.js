172aa89f63229ba76174775a178babc9b6080f88
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-18 23:49:31 -0300

    Utilizando switch

--- js/operadores e funcoes/operadorTernarioEswitch.js
+++ js/operadores e funcoes/operadorTernarioEswitch.js
@@ -3,4 +3,10 @@ let opcao = 2
 switch (opcao) {
     case 1 : 
         console.log("Saldo da conta")
+        break
+    case 2 :
+        console.log("Cartão de crédito")
+        break
+    default :
+        console.log("Falar com atendente")
 }
