aba9cecdfd658a2e93fada131f623fbcc63dd9e8
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-20 01:46:34 -0300

    Criado o for executando até 5

--- js/arrays e loops/doWhileFor.js
+++ js/arrays e loops/doWhileFor.js
@@ -10,10 +10,27 @@ while (numero <= 5) {
 
 // do...while
 
+/*
 let numero = 5
 do {
     console.log("Executou " + numero)
     numero--
 } while (numero >= 1)
+*/
+
+// for
+
+/*
+let numero = 5
+do {
+    console.log("Executou " + numero)
+    numero--
+} while (numero >= 1)
+*/
+
+for ( let numero = 1; numero <= 5; numero++) {
+    console.log("Executou " + numero)
+}
+
 
 
