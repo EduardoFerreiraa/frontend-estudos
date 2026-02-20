3b03a787747fa0552cbb57a2bcff1b6f05ed0a8b
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-20 01:42:46 -0300

    Criado do while

--- js/arrays e loops/doWhileFor.js
+++ js/arrays e loops/doWhileFor.js
@@ -1,8 +1,19 @@
 // While
 
+/*
 let numero = 1
 while (numero <= 5) {
     console.log("Executou " + numero)
     numero++
 }
+*/
+
+// do...while
+
+let numero = 5
+do {
+    console.log("Executou " + numero)
+    numero--
+} while (numero >= 1)
+
 
