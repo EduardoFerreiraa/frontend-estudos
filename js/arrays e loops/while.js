1a5c004ba004b4b05594ca259cc77ad225a88cc0
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-20 01:24:09 -0300

    Criando o loop while

---
+++ js/arrays e loops/while.js
@@ -0,0 +1,12 @@
+/*
+    while (condição) {
+        Executa enquanto a condição é verdadeira
+    }
+*/
+
+let numero = 1
+
+while( numero <= 4) {
+    console.log("pedaço de pizza " + numero)
+    numero = numero + 1
+}
