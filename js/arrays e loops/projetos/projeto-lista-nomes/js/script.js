035f517174193d173ff3f61eaa01254c40e73928
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-22 04:54:55 -0300

    Criado a function pesquisarNome( )

--- js/arrays e loops/projetos/projeto-lista-nomes/js/script.js
+++ js/arrays e loops/projetos/projeto-lista-nomes/js/script.js
@@ -10,6 +10,26 @@ let nomes = [
     "carlos"
 ]
 
+function pesquisarNome() {
+
+    let nomePesquisa = document.getElementById('campoNome').value
+    let itensLista = ''
+
+    for(indice in nomes) {
+
+        let nome = nomes[indice]
+        if (nomePesquisa == nome) {
+            itensLista += 
+            `<li class="list-group-item">
+                ${nome}
+            </li>`
+        }
+
+        document.getElementById('lista').innerHTML = itensLista
+    }
+
+}
+
 function carregarNomes() {
 
     let itensLista = ''
