d1b7ef8498d480f276f52d7ad8425f8ea413f4a8
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-23 00:47:07 -0300

    Usando uma função anonima

---
+++ js/arrays e loops/funcaoAnonimaEarrow.js
@@ -0,0 +1,17 @@
+/* // + Função literal
+
+function somar(a, b) {
+    console.log(a + b)
+}
+
+somar(1, 2)
+*/
+
+// Função anônima (sem nome)
+const somar = function(a, b) {
+    console.log(a + b)
+}
+somar(1, 2)
+
+// + Função arrow ( seta )
+
