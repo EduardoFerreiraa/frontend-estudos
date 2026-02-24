07e3417fa16d55d45e045c55a88cea90a7af4029
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-24 02:06:23 -0300

    Aplicando paradigma procedural

--- js/orientacao a objetos/pooPratica.js
+++ js/orientacao a objetos/pooPratica.js
@@ -3,5 +3,14 @@
 // Javascript é multi paradigma
 
 // Procedural
+function verificarDisponibilidade(q,o) {
+    let res = q - o
+    console.log("Disponíveis: " + res)
+}
+
+let quartos = 20
+let ocupados = 10
+verificarDisponibilidade(quartos, ocupados)
+
 
 // Orientado a objetos
