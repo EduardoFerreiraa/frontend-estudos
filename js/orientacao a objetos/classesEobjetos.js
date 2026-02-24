e3e36e2cfec19f5a1c0d596ce1f8445472e0d450
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-24 12:58:45 -0300

    Utilizando notação literal

--- js/orientacao a objetos/classesEobjetos.js
+++ js/orientacao a objetos/classesEobjetos.js
@@ -1,5 +1,19 @@
 // Notação Literal 
+const hotel = {
+    quartos: 20,
+    ocupados: 10,
+    piscinas: 2,
+    verificarDisponibilidade: function() {
+        let res = this.quartos - this.ocupados
+        return "Disponível: " + res
+    }
+}
 
+hotel.quartos = 25
+// hotel['quartos'] = 30
+delete hotel.piscinas
+console.log(hotel.quartos)
+console.log(hotel.piscinas)
 
 // Notação de construtor (objeto em branco)
 
