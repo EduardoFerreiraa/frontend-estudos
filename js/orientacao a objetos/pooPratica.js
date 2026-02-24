08b46a98717eb80bac9ac19fe045d2a40f6d92d0
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-24 02:02:28 -0300

    Create pooPratica.js

---
+++ js/orientacao a objetos/pooPratica.js
@@ -0,0 +1,7 @@
+// Paradigma -> exemplo ou padrão a ser seguido, não se trata de uma linguagem, mas a forma como você soluciona problemas usando uma linguagem de programação
+
+// Javascript é multi paradigma
+
+// Procedural
+
+// Orientado a objetos
