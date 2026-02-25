e42f815cdc84044c5bfbb552082cf2fc163619db
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-24 23:54:05 -0300

    Exemplo de diferentes características da classe Produto

--- js/orientacao a objetos/abstracao.js
+++ js/orientacao a objetos/abstracao.js
@@ -4,6 +4,7 @@ Modelo, Entidade, Identidade,
 Características e Ações
 */
 
+// Criando uma Classe
 class Carro {
     constructor() {
         this.marca = "Volkswagen",
@@ -11,11 +12,33 @@ class Carro {
         this.cor = "prata",
         this.placa = "EMJ-2565"
     }
+
+    // Exemplo - Ações
+    ligar() {
+
+    }
 }
 
+// Objeto
 const carro = new Carro()
 carro.modelo = "Golf"
 console.log(carro.modelo)
 
 const carro2 = new Carro()
 console.log(carro2.modelo)
+
+// Loja virtual
+class Produto {
+    constructor() {
+
+        // roupas
+        this.tamanho = "P",
+        this.cor = "vermelho",
+        this.preco = "45,90",
+
+        // Eletronicos
+        this.altura = "50cm",
+        this.largura = "30cm"
+
+    }
+}
