8438384c03a9d151e4901b35ff4c5aa4dc881a25
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 12:59:58 -0300

    Create file heranca.js

---
+++ js/orientacao a objetos/heranca.js
@@ -0,0 +1,9 @@
+// Herança - Reutilização e manutenção
+// Classe - Cao Passaro 
+class Cao {
+    constructor() {
+        this.cor = "",
+        this.tamanho = 0,
+        this.peso = 0
+    }
+}
