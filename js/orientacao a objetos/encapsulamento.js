709a311c6d45f42f8fda8564dff3d9a591be72f5
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 01:31:17 -0300

    Criado getters e setters e métodos com parametros

--- js/orientacao a objetos/encapsulamento.js
+++ js/orientacao a objetos/encapsulamento.js
@@ -37,14 +37,49 @@ filtro.pretoEbranco( imagem )
 
 // Encapsulamento, modificadores de acesso e getters e setter
 class ContaBancaria {
+    
     constructor() {
-        this._numeroConta = 0,
-        this.saldo = 0
+        this._numeroConta = 80,
+        this._saldo = 0
     }
+
+    sacar(valorSaque) {
+        this._saldo = this.saldo - valorSaque
+    }
+
+    depositar(valorDeposito) {
+        this._saldo = this.saldo + valorDeposito
+    }
+
+
+    get saldo() {
+        return this._saldo
+    }
+
+    set saldo(novoSaldo) {
+        if (novoSaldo > 0) {
+            this._saldo = novoSaldo
+        }
+    }
+
+    get numeroConta() {
+        // Verificar se o usuário está logado
+        return "Número: " + this._numeroConta 
+    }
+
+    set numeroConta( numero ) {
+        if (numero > 0) {
+            this._numeroConta = numero
+        }
+    }
+
 }
 
 const conta = new ContaBancaria()
-conta._numeroConta = 50
+//conta.numeroConta = 60
+conta.saldo = 500 // seguro
+conta.sacar(50) // 450
+conta.depositar(100) // 550
 
-console.log(conta._numeroConta)
+console.log(conta.saldo)
 
