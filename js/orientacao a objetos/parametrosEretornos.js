99b195d3698e679beeb1ea8fa1f614ac02f71509
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 00:14:01 -0300

    Calculando o desconto do cupom com if else e parametros

--- js/orientacao a objetos/parametrosEretornos.js
+++ js/orientacao a objetos/parametrosEretornos.js
@@ -3,14 +3,15 @@
 class Usuario {
     constructor() {
         this.email = "",
-        this.senha = ""
+        this.senha = "",
+        this.subtotalCompra = 0
     }
 
     login() {
         let emailBd = "ja@gmail.com"
         let senhaBd = "1234"
 
-        if( senhaBd == this.senha) {
+        if ( senhaBd == this.senha) {
             //console.log("senha válida")
             return "senha válida"
         } else {
@@ -18,11 +19,29 @@ class Usuario {
             return "senha inválida"
         }
     }
+
+    calcularDesconto(cupom) {
+        
+        let desconto = 0
+        if ( cupom == "DESC20") {
+            desconto = 20
+        } else if (cupom == "FESTA10") {
+            desconto = 10
+        }
+        
+        return this.subtotalCompra - desconto  
+    }
 }
 
 const usuario = new Usuario()
+usuario.subtotalCompra = 279
+usuario.calcularDesconto("FESTA10")
+
+/*
 usuario.email = "ja@gmail.com"
 usuario.senha = "1234"
 
 let mensagem = usuario.login()
 console.log(mensagem)
+*/
+
