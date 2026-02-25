5f57bb342cf69581098327f6d402c726ce2c35a1
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 13:00:37 -0300

    Criado a classe cao

--- js/orientacao a objetos/heranca.js
+++ js/orientacao a objetos/heranca.js
@@ -1,9 +1,19 @@
 // Herança - Reutilização e manutenção
 // Classe - Cao Passaro 
+
 class Cao {
     constructor() {
         this.cor = "",
         this.tamanho = 0,
         this.peso = 0
     }
+
+    corre() {
+        console.log("correr")
+    }
+
+    dormir() {
+        console.log("dormir")
+    }
 }
+
