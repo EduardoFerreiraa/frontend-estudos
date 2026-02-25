0359844dc3a856e1bf4bc2843805aaa7f0bc3262
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 00:55:29 -0300

    Utilizando o _ no numeroConta para que não tenha acesso

--- js/orientacao a objetos/encapsulamento.js
+++ js/orientacao a objetos/encapsulamento.js
@@ -38,11 +38,13 @@ filtro.pretoEbranco( imagem )
 // Encapsulamento, modificadores de acesso e getters e setter
 class ContaBancaria {
     constructor() {
-        this.numeroConta = 0,
+        this._numeroConta = 0,
         this.saldo = 0
     }
 }
 
 const conta = new ContaBancaria()
-conta.numeroConta = 50
+conta._numeroConta = 50
+
+console.log(conta._numeroConta)
 
