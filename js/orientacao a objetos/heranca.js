4e99e4c0a4fab5b65911f4fa4c4b2899dc56f34f
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 13:04:20 -0300

    Criado a classe Passaro e método latir( ) na classe Cao

--- js/orientacao a objetos/heranca.js
+++ js/orientacao a objetos/heranca.js
@@ -15,5 +15,28 @@ class Cao {
     dormir() {
         console.log("dormir")
     }
+
+    latir() {
+        console.log("latir")
+    }
 }
 
+class Passaro {
+    constructor() {
+        this.cor = "",
+        this.tamanho = 0,
+        this.peso = 0
+    }
+
+    corre() {
+        console.log("correr")
+    }
+
+    dormir() {
+        console.log("dormir")
+    }
+
+    voar() {
+        console.log("voar")
+    }
+}
