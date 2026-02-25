f5933feff9a0cbcbba166dc4dafeaad627df244b
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-02-25 00:02:09 -0300

    Criado let emailBd e senhaBd, verificando se a senha é válida ou inválida com if else

--- js/orientacao a objetos/parametrosEretornos.js
+++ js/orientacao a objetos/parametrosEretornos.js
@@ -5,8 +5,23 @@ class Usuario {
         this.email = "",
         this.senha = ""
     }
+
+    login() {
+
+        let emailBd = "ja@gmail.com"
+        let senhaBd = "1234"
+
+        if( senhaBd == this.senha) {
+            console.log("senha válida")
+        } else {
+            console.log("senha inválida")
+        }
+
+    }
 }
 
 const usuario = new Usuario()
 usuario.email = "ja@gmail.com"
 usuario.senha = "1234"
+
+usuario.login()
