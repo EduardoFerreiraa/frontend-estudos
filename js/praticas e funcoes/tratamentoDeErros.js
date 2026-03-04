2d899c29588763dfac3bcdd9ea0b0d522052caef
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-04 01:15:04 -0300

    Criando uma function e objeto produto

---
+++ js/praticas e funcoes/tratamentoDeErros.js
@@ -0,0 +1,11 @@
+// Tratamento de erros com Try Catch
+function contarQuantidadeLetras(produto) {
+    console.log(produto.nome.length)
+}
+
+const produto = {
+    nome: "Notebook",
+    preco: 1220
+}
+
+contarQuantidadeLetras(produto)
