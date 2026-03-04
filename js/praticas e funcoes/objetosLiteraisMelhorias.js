329ff902a337ee116431e0a318a3d08310b73135
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-04 00:51:24 -0300

    Alterando valor do preco

--- js/praticas e funcoes/objetosLiteraisMelhorias.js
+++ js/praticas e funcoes/objetosLiteraisMelhorias.js
@@ -3,13 +3,31 @@
 let nome = "Notebook"
 let preco = 1200
 
-const produto = {
 
+const produto = {
     nome, // nome: nome 
     preco,
     exibirProduto() {
-        console.log(`${this.nome}, ${this.preco} `)
+        console.log(`${this.nome}, ${this.preco}, ${this.categoria} `)
     }
 }
-produto.exibirProduto()
-console.log(produto.nome)
+
+
+const pro = produto 
+pro.preco = 2000
+
+console.log(pro.preco)
+console.log(produto.preco)
+
+/*
+produto.categoria = "Eletrônicos"
+produto.exbirPreco = function() {
+    `Preço: ${this.preco}`
+}
+produto.nome = "Notebook"
+produto.exbirPreco()
+*/
+
+
+//produto.exibirProduto()
+// console.log(produto.categoria)
