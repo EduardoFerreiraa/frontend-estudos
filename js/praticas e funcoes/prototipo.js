7aa69b2597013a067e510fdcc38672525181136f
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-05 00:45:01 -0300

    Todos os objetos são descendentes de prototype

--- js/praticas e funcoes/prototipo.js
+++ js/praticas e funcoes/prototipo.js
@@ -1,7 +1,11 @@
 // Descendentes de Objetc
+Object.prototype
+
 
 const obj = {
 
 }
 
-console.log(obj.toString())
+console.log( obj.toString() )
+console.log( obj.__proto__ )
+console.log( obj.__proto__ == Object.prototype )
