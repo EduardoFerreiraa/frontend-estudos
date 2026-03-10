8b1c3e5391e23ed42c40cae4a6f876b77d45c5a7
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 00:55:23 -0300

    Utilizando split no nome

--- js/funcoes nativas/manipulacaoDeTextos.js
+++ js/funcoes nativas/manipulacaoDeTextos.js
@@ -4,12 +4,14 @@
     charAt(){}
 } */
 
-let nome = "Carro do Eduardo"
+// let nome = "Carro do Eduardo"
 // let resultado = nome.length
 // let resultado = nome.charAt(0)
 // let resultado = nome.replace("Eduardo", "Pedro")
-let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
-let resultado = frase.substr(0, 30) + "..."
+//let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+// let resultado = frase.substr(0, 30) + "..."
 
-console.log(resultado)
+let nome = "eduardo-ferreira"
+let resultado = nome.split("-")
+console.log(resultado[0])
 
