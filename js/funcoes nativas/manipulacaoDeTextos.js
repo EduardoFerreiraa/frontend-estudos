226fb7808a59acbc65fe47419e53c1f6b3c180ff
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 00:40:56 -0300

    Utilizando charAt

---
+++ js/funcoes nativas/manipulacaoDeTextos.js
@@ -0,0 +1,12 @@
+/* const nome = {
+    texto: "ja",
+    length: 2,
+    charAt(){}
+} */
+
+let nome = "Eduardo"
+// let resultado = nome.length
+let resultado = nome.charAt(0)
+
+console.log(resultado)
+
