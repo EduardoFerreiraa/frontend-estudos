8597e2a31d71c43adf9c818526ae9cf239a3253d
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 00:58:46 -0300

    Utilizando o método slice

--- js/funcoes nativas/manipulacaoDeTextos.js
+++ js/funcoes nativas/manipulacaoDeTextos.js
@@ -4,14 +4,24 @@
     charAt(){}
 } */
 
-// let nome = "Carro do Eduardo"
-// let resultado = nome.length
-// let resultado = nome.charAt(0)
-// let resultado = nome.replace("Eduardo", "Pedro")
-//let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
-// let resultado = frase.substr(0, 30) + "..."
+/* let nome = "Carro do Eduardo"
+ let resultado = nome.length
+ let resultado = nome.charAt(0)
+ let resultado = nome.replace("Eduardo", "Pedro")
+ let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+ let resultado = frase.substr(0, 30) + "..." */
 
+ /*
 let nome = "eduardo-ferreira"
 let resultado = nome.split("-")
 console.log(resultado[0])
+*/
+
+let nome = "eduardo"
+let sobrenome = "ferreira"
+let s = sobrenome.slice(0, 6)
+let resultado = nome + s + "@empresa.com.br"
+
+console.log(resultado)
+
 
