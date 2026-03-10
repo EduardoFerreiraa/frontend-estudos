629a5420e08bced78a07b205ded146bafd2422f0
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 01:14:43 -0300

    Utilizando min( )

--- js/funcoes nativas/matematica.js
+++ js/funcoes nativas/matematica.js
@@ -4,5 +4,6 @@ let numero = 280.50;
 // let resultado = Math.ceil(numero) // arredonda para cima
 // let resultado = Math.floor(numero) // arredonda para baixo
 // let resultado = Math.round(numero) // arredonda 
-let resultado = Math.random(numero) // 0 e 1
+// let resultado = Math.random(numero) // 0 e 1
+let resultado = Math.min(15, 10)
 console.log(resultado)
