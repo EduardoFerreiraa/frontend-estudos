acd5ff7a5f2321975a527b9c5a72b0726e49cc4e
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-09 23:58:07 -0300

    Deixando um valor padrão na function somar

--- js/funcoes nativas/parametrosEretornosDeFuncoes.js
+++ js/funcoes nativas/parametrosEretornosDeFuncoes.js
@@ -1,6 +1,6 @@
 // Parâmetros padrão 
 
-function somar(numero1, numero2) {
+/*function somar(numero1, numero2) {
     numero1 = isNan(numero1) ? 0 : numero1
     numero2 = isNan(numero2) ? 0 : numero2
 
@@ -9,4 +9,11 @@ function somar(numero1, numero2) {
 
 somar(2, "Eduardo");
 
-// NaN -> Not a Number (não é um número)
+// NaN -> Not a Number (não é um número) */
+
+// Valor padrão do es2015
+function somar(numero1 = 0, numero2 = 0) {
+    return numero1 + numero2;
+}
+
+somar(2, 10);
