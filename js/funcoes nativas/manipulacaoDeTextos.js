0edb73c65edca05874e34136a54ae03ac4c6c652
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 00:52:46 -0300

    Utilizando substr

--- js/funcoes nativas/manipulacaoDeTextos.js
+++ js/funcoes nativas/manipulacaoDeTextos.js
@@ -7,7 +7,9 @@
 let nome = "Carro do Eduardo"
 // let resultado = nome.length
 // let resultado = nome.charAt(0)
-let resultado = nome.replace("Eduardo", "Pedro")
+// let resultado = nome.replace("Eduardo", "Pedro")
+let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+let resultado = frase.substr(0, 30) + "..."
 
 console.log(resultado)
 
