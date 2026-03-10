3797d38a1d37cb5daccc8e43b168ad26a8755f49
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-09 23:54:35 -0300

    Utilizando isNan

---
+++ js/funcoes nativas/parametrosEretornosDeFuncoes.js
@@ -0,0 +1,12 @@
+// Parâmetros padrão 
+
+function somar(numero1, numero2) {
+    numero1 = isNan(numero1) ? 0 : numero1
+    numero2 = isNan(numero2) ? 0 : numero2
+
+    return numero1 + numero2;
+}
+
+somar(2, "Eduardo");
+
+// NaN -> Not a Number (não é um número)
