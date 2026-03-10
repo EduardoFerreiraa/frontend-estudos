847922bc8b8d238df04d8e846c5de7ac9d7bf514
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 01:34:56 -0300

    Utilizando setHours, setMinutes e setSeconds para mudar as horas, minutos e segundos

--- js/funcoes nativas/datas.js
+++ js/funcoes nativas/datas.js
@@ -15,7 +15,9 @@ let seconds = data.getSeconds()
 console.log(`Hora: ${hour}:${min}:${seconds}`)
 */
 
+
 // Operações com datas
+/*
 data.setDate(data.getDate() + 6)
 data.setMonth(data.getMonth() + 8)
 data.setFullYear(data.getFullYear() + 1)
@@ -24,3 +26,14 @@ let d = data.getDate()
 let m = data.getMonth() + 1
 let y = data.getFullYear()
 console.log(`Data: ${d}/${m}/${y}`)
+*/
+
+data.setHours(data.getHours() + 1)
+data.setMinutes(data.getMinutes() + 15)
+data.setSeconds(data.getSeconds() + 50)
+
+let hour = data.getHours()
+let min = data.getMinutes()
+let seconds = data.getSeconds()
+
+console.log(`Hora: ${hour}:${min}:${seconds}`)
