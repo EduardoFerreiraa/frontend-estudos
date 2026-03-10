76b791491db066cfe774e779d0553a6d0ad468a8
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 00:11:54 -0300

    Trabalhando com parâmetros e retornos opcionais

--- js/funcoes nativas/parametrosEretornosDeFuncoes.js
+++ js/funcoes nativas/parametrosEretornosDeFuncoes.js
@@ -12,8 +12,16 @@ somar(2, "Eduardo");
 // NaN -> Not a Number (não é um número) */
 
 // Valor padrão do es2015
-function somar(numero1 = 0, numero2 = 0) {
+/* function somar(numero1 = 0, numero2 = 0) {
     return numero1 + numero2;
+} 
+
+somar(2, 10); */
+
+// Parâmetros e retornos opcionais 
+function calcularSalario(salario, desconto) {
+    desconto = isNan(desconto) ? 0 : desconto;
+    return salario - desconto;
 }
 
-somar(2, 10);
+let resultado = calcularSalario(100, 20);
