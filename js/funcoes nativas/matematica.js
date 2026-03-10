d54849e55ec7b104cfa827be2ec35b05d2ca260f
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 01:13:14 -0300

    Arredondando com round( )

--- js/funcoes nativas/matematica.js
+++ js/funcoes nativas/matematica.js
@@ -2,5 +2,6 @@ let numero = 280.50;
 
 // Math.PI
 // let resultado = Math.ceil(numero) // arredonda para cima
-let resultado = Math.floor(numero) // arredonda para baixo
+// let resultado = Math.floor(numero) // arredonda para baixo
+let resultado = Math.round(numero) // arredonda 
 console.log(resultado)
