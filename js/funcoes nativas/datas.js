3913a997279a927966f49b518ce30b6bba5920b8
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-10 01:28:56 -0300

    Imprimindo horas

--- js/funcoes nativas/datas.js
+++ js/funcoes nativas/datas.js
@@ -6,3 +6,9 @@ let m = data.getMonth() + 1
 let y = data.getFullYear()
 
 console.log(`Data: ${d}/${m}/${y}`)
+
+let hour = data.getHours()
+let min = data.getMinutes()
+let seconds = data.getSeconds()
+
+console.log(`Hora: ${hour}:${min}:${seconds}`)
