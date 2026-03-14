28faca5e84e1fef67b9fd18d5b729c1928f011d0
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-14 00:10:09 -0300

    Removendo o primeiro elemento do Array

--- js/arrays/metodosUteis.js
+++ js/arrays/metodosUteis.js
@@ -2,4 +2,6 @@ const estados = ["São Paulo", "Ceará", "Rio de Janeiro", "Bahia", "Minas Gerai
 
 /* Remove elementos de um Array */
 estados.pop() // remove o último elemento do Array
+estados.shift() // remove o primeiro elemento 
+
 console.log(estados)
