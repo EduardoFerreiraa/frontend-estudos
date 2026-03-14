8810890ea7f7850ba877295aece389ae607d64f6
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-14 00:09:38 -0300

    Removendo o último elemento do Array

--- js/arrays/metodosUteis.js
+++ js/arrays/metodosUteis.js
@@ -1,3 +1,5 @@
 const estados = ["São Paulo", "Ceará", "Rio de Janeiro", "Bahia", "Minas Gerais"]
 
 /* Remove elementos de um Array */
+estados.pop() // remove o último elemento do Array
+console.log(estados)
