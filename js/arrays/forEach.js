4a27aff57c525ab9b509a805e59ad13cfb487261
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-14 01:51:58 -0300

    Criando uma function e usando this

--- js/arrays/forEach.js
+++ js/arrays/forEach.js
@@ -4,10 +4,17 @@ const lista = ["Jamilton", "Ana", "Marcela", "Pedro"];
 for (indice in lista) {
     console.log(lista[indice])
 } 
-*/
+
 
 const percorrer = function(item, i, arr) {
     console.log(item);
 }
 
 lista.forEach(percorrer);
+*/
+
+Array.prototype.percorrer = function() {
+    console.log(this);
+}
+
+lista.percorrer()
