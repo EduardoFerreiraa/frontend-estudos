dd750427c54fe7ab31412504b702289b7bc9b0d0
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-19 23:54:37 -0300

    Atualizando

--- js/funcoes nativas/datas.js
+++ js/funcoes nativas/datas.js
@@ -5,16 +5,13 @@ data.toString()
 let d = data.getDate()
 let m = data.getMonth() + 1
 let y = data.getFullYear()
-
 console.log(`Data: ${d}/${m}/${y}`)
 
 let hour = data.getHours()
 let min = data.getMinutes()
 let seconds = data.getSeconds()
-
 console.log(`Hora: ${hour}:${min}:${seconds}`)
 */
-
 
 // Operações com datas
 /*
