f2f67c729873ecdb5863a0de6300391800677405
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-04 15:09:25 -0300

    feat: utilizando ES Modules

--- typescript/src/script1.js
+++ typescript/src/script1.js
@@ -1,4 +1,7 @@
-function sum(a, b) {
-    return a + b;
+export function sum(a, b) {
+    console.log('Dentro do escopo de módulo');
+    return {result: a + b};
 }
+
+
 
--- typescript/src/script2.js
+++ typescript/src/script2.js
@@ -1,4 +1,5 @@
 function sum(a, b) {
+    console.log('Dentro do escopo global');
     return a + b;
 }
 
