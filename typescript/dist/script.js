b44df1037b13deb0a9e4e99a90a3717dbac30fcc
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-04 19:03:09 -0300

    feat: type inference

--- typescript/dist/script.js
+++ typescript/dist/script.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sum = sum;
 function sum(a, b) {
-    console.log('Dentro do escopo de módulo');
-    return { result: a + b };
+    console.log("Dentro do escopo de módulo");
+    return {
+        result: a + b,
+    };
 }
---
+++ typescript/src/modules/basic-types/type-inference.ts
@@ -0,0 +1,5 @@
+let product = "Fritadeira Elétrica";
+
+product = 750;
+
+export default product;
