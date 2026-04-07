ab6ce0fac3b6a15fb507c108b44d9c268acc9c50
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-07 01:13:00 -0300

    feat: utilizando type annotation

---
+++ typescript/dist/modules/basic-types/type-annotation.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.display = display;
+// let product: string = "Eduardo";
+let product = "Eduardo";
+let price = 647.52;
+// product.toUpperCase();
+// price.toFixed(0);
+function display(product, price) {
+    console.log(product.toUpperCase(), price.toFixed(0));
+}
+display(product, price);
--- typescript/src/modules/basic-types/type-annotation.ts
+++ typescript/src/modules/basic-types/type-annotation.ts
@@ -1,4 +1,12 @@
 // let product: string = "Eduardo";
 let product = "Eduardo";
+let price = 647.52;
 
-product.toUpperCase();
+// product.toUpperCase();
+// price.toFixed(0);
+
+export function display(product: string, price: number) {
+  console.log(product.toUpperCase(), price.toFixed(0));
+}
+
+display(product, price);
