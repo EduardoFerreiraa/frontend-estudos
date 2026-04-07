a35815170189d90df85864958f287b095b89338f
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-07 02:40:25 -0300

    feat: utilizando type array no typescript

--- typescript/dist/modules/basic-types/type-array.js
+++ typescript/dist/modules/basic-types/type-array.js
@@ -1,13 +1,12 @@
+"use strict";
 // let films = ["Duna: parte 2", "Divertida Mente 2"];
-
-let films: string[] = ["Duna: parte 2", "Divertida Mente 2"];
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toUpperCaseStrings = toUpperCaseStrings;
+let films = ["Duna: parte 2", "Divertida Mente 2"];
 // let numbers: number[] = [10, 20, 30, 40, 50];
-
 // let films: Array<string> = ["Duna: parte 2", "Divertida Mente 2"];
 // let numbers: Array<number> = [10, 20, 30, 40, 50];
-
-export function toUpperCaseStrings(arr: string[]) {
+function toUpperCaseStrings(arr) {
     return arr.map(value => value.toUpperCase());
 }
-
 console.log(toUpperCaseStrings(films));
