45e855f7f51cfd9207ed0790a8de619ad57f610e
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-07 02:48:27 -0300

    feat: utilizando union type em funções

---
+++ typescript/src/modules/basic-types/union-types.ts
@@ -0,0 +1,6 @@
+let shoppingCart = [200.75, 150.12, "33.90", "44", "not defined"];
+
+export function totalize(values: Array<(number | string | null | object)>) {}
+// export function totalize(values: (number | string)[]) {}
+
+totalize(shoppingCart);
