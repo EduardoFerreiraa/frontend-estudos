d356ef9d6b37c57ae5ca8b686a0e2bb7ec5bb995
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-07 23:16:41 -0300

    feat: criando um type programmer

--- typescript/src/modules/basic-types/type-alias.ts
+++ typescript/src/modules/basic-types/type-alias.ts
@@ -1,9 +1,16 @@
-export function showProgrammer(programmer: {
+type Programmer = {
   name: string;
   age?: number;
   skills?: string[];
-}) {
+  contact: { email: string; phone: string };
+};
+
+export function showProgrammer(programmer: Programmer) {
   console.log(programmer);
 }
 
-showProgrammer({ name: "Leonardo", skills: ["PHP"] });
+showProgrammer({
+  name: "Maria",
+  skills: ["PHP"],
+  contact: { email: "eduardoferreira@gmail.com", phone: "+55 11 98888-7777" },
+});
