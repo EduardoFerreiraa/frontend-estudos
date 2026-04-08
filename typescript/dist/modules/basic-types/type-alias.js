d4eb23cabe4ab11a1abbce256e8fdebf94ba9aa7
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-07 23:20:29 -0300

    feat: criando type salary recebendo number e string

---
+++ typescript/dist/modules/basic-types/type-alias.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.showProgrammer = showProgrammer;
+function showProgrammer(programmer) {
+    console.log(programmer);
+}
+showProgrammer({
+    name: "Maria",
+    skills: ["PHP"],
+    contact: { email: "eduardoferreira@gmail.com", phone: "+55 11 98888-7777" },
+});
--- typescript/src/modules/basic-types/type-alias.ts
+++ typescript/src/modules/basic-types/type-alias.ts
@@ -1,8 +1,10 @@
+type Salary = number | string;
 type Programmer = {
   name: string;
   age?: number;
   skills?: string[];
   contact: { email: string; phone: string };
+  salary?: Salary;
 };
 
 export function showProgrammer(programmer: Programmer) {
