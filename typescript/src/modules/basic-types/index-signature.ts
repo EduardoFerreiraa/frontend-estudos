ce45b41df6501da0db13bf31aa917cddb5ea46af
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-07 23:32:48 -0300

    feat: criado index-signature.ts

---
+++ typescript/src/modules/basic-types/index-signature.ts
@@ -0,0 +1,26 @@
+// Fetch endpoint /movies
+
+let movies = {
+  movie1: {
+    title: "A Origem",
+    year: 2010,
+    isFavorite: true,
+    genre: "Ficção Científica",
+    director: "Christopher Nolan",
+  },
+
+  movie2: {
+    title: "Um sonho de liberdade",
+    year: 1994,
+    isFavorite: true,
+    genre: "Drama",
+    runtime: 142,
+  },
+
+  movie3: {
+    title: "The Godfather",
+    year: 1972,
+    isFavorite: false,
+    genre: "Crime",
+  },
+};
