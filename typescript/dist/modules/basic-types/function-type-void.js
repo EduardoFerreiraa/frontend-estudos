8b5c6636912ffdd25434fd24f68b3e16dae58322
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-08 23:40:10 -0300

    feat: calculando o valor dos itens

---
+++ typescript/dist/modules/basic-types/function-type-void.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateTotal = calculateTotal;
+//TODO criar variável do tipo ShoppingCart contendo CartItem
+const shoppingCart = {
+    cartItems: [
+        { id: 1000, price: 200 },
+        { id: 1070, price: 300 },
+        { id: 3000, price: 500 },
+    ],
+};
+//TODO função do tipo void para somar os valores dos itens
+function calculateTotal(shoppingCart) {
+    const total = shoppingCart.cartItems.reduce((acc, item) => acc + item.price, 0);
+    console.log(`Total do carrinho: R$ ${total.toFixed(2)}`);
+}
+calculateTotal(shoppingCart);
