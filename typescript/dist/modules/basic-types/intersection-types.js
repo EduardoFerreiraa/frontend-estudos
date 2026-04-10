e8a5fe827de9f6d9501cf7436e4264dc4d4ae6d4
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-10 03:10:45 -0300

    feat: intersection types

---
+++ typescript/dist/modules/basic-types/intersection-types.js
@@ -0,0 +1,13 @@
+export const bootstrap = () => {
+    const employee = {
+        name: "Jorge",
+        age: 25,
+        departament: "TI",
+    };
+    const customer = {
+        name: "Maria",
+        age: 28,
+        departament: "Contabilidade",
+        wishList: ["Smartphone", "Laptop", "Headphones"],
+    };
+};
--- typescript/src/modules/basic-types/intersection-types.ts
+++ typescript/src/modules/basic-types/intersection-types.ts
@@ -1,3 +1,31 @@
-export const bootstrap = ():void => {
-    
-}
+export const bootstrap = (): void => {
+  type Person = {
+    name: string;
+    age: number;
+  };
+
+  type Employee = {
+    departament: string;
+  };
+
+  type Customer = {
+    wishList: string[];
+  };
+
+  type EmployeeDatails = Person & Employee;
+
+  const employee: EmployeeDatails = {
+    name: "Jorge",
+    age: 25,
+    departament: "TI",
+  };
+
+  type CustomerDetails = Person & Customer & Employee;
+
+  const customer: CustomerDetails = {
+    name: "Maria",
+    age: 28,
+    departament: "Contabilidade",
+    wishList: ["Smartphone", "Laptop", "Headphones"],
+  };
+};
