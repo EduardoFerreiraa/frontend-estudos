6a7c70a6fb6ababbed03a1aab691b460f5dafdd1
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-20 22:31:40 -0300

    feat: Array.isArray( )

---
+++ typescript/dist/modules/narrowing/array-IsArray.js
@@ -0,0 +1,11 @@
+export const bootstrap = () => {
+    const arr = [1, 2, 3];
+    console.log("Typeof de array: ", typeof arr);
+    console.log("Instanceof de array: ", arr instanceof Array);
+    console.log("Array.isArray(): ", Array.isArray(arr));
+    if (Array.isArray(arr)) {
+        arr?.map((item) => {
+            console.log(item);
+        });
+    }
+};
--- typescript/dist/script.js
+++ typescript/dist/script.js
@@ -1,2 +1,2 @@
-import { bootstrap } from "./modules/narrowing/in.js";
+import { bootstrap } from "./modules/narrowing/array-IsArray.js";
 export { bootstrap };
