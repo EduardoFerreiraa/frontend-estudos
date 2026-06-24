cac5b8ef9716d9ac8d1ad4adf8537625b0b2a920
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-24 02:15:47 -0300

    projeto next e estilo css

---
+++ react-e-next/react-fundamentos/src/app/fundamentos/2_propriedades/page.tsx
@@ -0,0 +1,15 @@
+import { Acomodacao, Cabecalho, Menu } from "@/src/components/interface";
+
+export default function Page() {
+    return (
+        <div>
+            <h1 className="bg-blue-500">Título propriedades (APP)</h1>
+            <Cabecalho titulo="São Roque, Brasil, teste"
+            preco="R$350"/>
+            <hr />
+            <Acomodacao />
+            <Cabecalho titulo="Campos do Jordão, Brasil"
+            preco="R$500"/>
+        </div>
+    )
+}
---
+++ react-e-next/react-fundamentos/src/app/fundamentos/3_estilo/page.tsx
@@ -0,0 +1,10 @@
+// import "../../estilo.css"
+import "@/src/app/estilo.css"
+
+export default function Page() {
+    return (
+        <div>
+            <h1 className="titulo">Título 3_estilo</h1>
+        </div>
+    )
+}
