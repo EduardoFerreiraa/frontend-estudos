c0739ff6278bdde9b4a672a718ef4e4d484e2f33
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-25 01:07:17 -0300

    Navegação entre páginas

--- react-e-next/react-fundamentos/src/app/acomodacoes/page.tsx
+++ react-e-next/react-fundamentos/src/app/acomodacoes/page.tsx
@@ -5,13 +5,13 @@ export default function Pagina() {
     return (
         <div>
             <Cabecalho titulo="AirBnb"
-             subtitulo="Início"
+             subtitulo="Minhas acomodações"
             />
 
             <Menu />
 
             <Conteudo titulo="teste">
-                <h1>Página início</h1>
+                <h1>Página Acomodações</h1>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aperiam eius rem quae dolorum consequatur voluptatem? Voluptates dolorum, cupiditate possimus cumque neque corporis asperiores voluptas ipsam quam recusandae laborum! Maxime.
                 </p>
