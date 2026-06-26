59cdb56d802d36d3bedfe239ec6c05c04f60cbaa
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-26 02:42:36 -0300

    componentes

---
+++ react-e-next/react-fundamentos/src/app/fundamentos/10_componentes/page.tsx
@@ -0,0 +1,11 @@
+export default function Aplicacao() {
+    return (
+        <div className="p-2">
+
+            <button className="btn-sucesso">Sucesso</button>
+            <button className="btn-informacoes">Informações</button>
+            <button className="btn-erro">Erro</button>
+
+        </div>
+    )
+}
