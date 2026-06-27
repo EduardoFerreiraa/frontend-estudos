ac07078f1e3d5ee5a0748f2b03cacbd91ccb8fa2
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-27 02:04:47 -0300

    Posicionamentos Tailwind

--- react-e-next/react-fundamentos/src/app/fundamentos/13_posicionamento/page.tsx
+++ react-e-next/react-fundamentos/src/app/fundamentos/13_posicionamento/page.tsx
@@ -2,18 +2,11 @@ import Image from "next/image"
 
 export default function Aplicacao() {
     return (
-        <div className="border">
-            
-            <Image
-                className="p-3 m-2.5 float-left"
-                src="/img/cidade.jpg"
-                alt="Descrição"
-                width={350}
-                height={320}
-            />
+        <div className="border p-3 m-2">
+            <p className="bg-orange-400">ITEM DE DESTAQUE sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
+            <p className="bg-purple-400 m-2 relative top-4 left-4 bottom-4">Item 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
+            <p className="bg-gray-400 m-2">Item 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
 
         </div>
     )
