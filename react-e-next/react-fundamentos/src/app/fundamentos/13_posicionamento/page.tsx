9f6fb3910717777f0d30bb367460d81763c3335a
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-27 20:25:41 -0300

    Positions absolute e sticky

--- react-e-next/react-fundamentos/src/app/fundamentos/13_posicionamento/page.tsx
+++ react-e-next/react-fundamentos/src/app/fundamentos/13_posicionamento/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image"
-
 export default function Aplicacao() {
     return (
         <div className="border p-3 m-2">
-            <p className="bg-orange-400">ITEM DE DESTAQUE sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
+            {/* <p className="bg-orange-400 m-2 fixed bottom-2"> */}
 
-            <p className="bg-purple-400 m-2 relative top-4 left-4 bottom-4">Item 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
-            <p className="bg-gray-400 m-2">Item 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta!</p>
+            <p className="bg-orange-400 m-2 sticky top-4 z-0">
+                ITEM DE DESTAQUE sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur alias accusantium culpa labore, et, quasi nihil recusandae cupiditate voluptatem nobis praesentium eius magni, incidunt iure eum neque reiciendis! Doloremque, nesciunt!
+            </p>
+
+            <p className="bg-purple-400 m-2 sticky top-4 z-10">Item 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolor rem asperiores perferendis deserunt ipsam suscipit earum amet fugiat atque fugit fuga itaque ipsa, quidem quisquam. Doloribus rerum velit voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum hic dolores aspernatur, quas facilis magni id provident rem. Eos repellendus praesentium pariatur facere beatae ipsum qui officiis et perspiciatis culpa.</p>
+
+            <p className="bg-gray-400 m-2 sticky top-4 z-20">Item 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatibus aperiam eaque temporibus, cupiditate, corrupti accusantium dolore quidem, ratione nemo voluptas nam animi molestias? Enim rem minima sit doloremque soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eveniet laboriosam alias enim ullam natus fuga quas nostrum corporis quos facere, doloremque delectus? Placeat error nesciunt minima sapiente deserunt voluptatem.</p>
 
         </div>
     )
