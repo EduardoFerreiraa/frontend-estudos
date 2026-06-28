07465c56f5a4dddd73d0d8a73d0d7b9ec5a57e21
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-27 21:12:32 -0300

    Responsividade em Tailwind

---
+++ next-e-tailwind/next-tailwind/src/app/1_responsividade/page.tsx
@@ -0,0 +1,16 @@
+export default function Responsividade() {
+    return (
+        <div>
+            <h1 
+            className="
+            bg-slate-500 
+            md:bg-orange-300
+            lg:bg-blue-800
+            xl:bg-yellow-400
+            2xl:bg-purple-900
+            ">
+                Responsividade
+            </h1>
+        </div>
+    )
+}
