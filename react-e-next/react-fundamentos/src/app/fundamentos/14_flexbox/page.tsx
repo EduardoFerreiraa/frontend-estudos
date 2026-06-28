a84788d104ecf5df1b30e364eaa630a8659db0ce
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-27 21:01:10 -0300

    Flexbox em Tailwind

---
+++ react-e-next/react-fundamentos/src/app/fundamentos/14_flexbox/page.tsx
@@ -0,0 +1,14 @@
+export default function Aplicacao() {
+    return (
+
+        // flex-row-reverse
+        <div className="border p-3 flex flex-row flex-wrap gap-3">
+            <div className="caixa basis-40">Caixa 1</div>
+            <div className="caixa basis-80">Caixa 2</div>
+            <div className="caixa basis-40">Caixa 3</div>
+            <div className="caixa basis-40">Caixa 4</div>
+            <div className="caixa basis-80">Caixa 5</div>
+            <div className="caixa basis-40">Caixa 6</div>
+        </div>
+    )
+}
