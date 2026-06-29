e68fff0002aa0c4d78680cba18e68c5b720244a6
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-29 03:32:37 -0300

    grow e shrink

--- react-e-next/react-fundamentos/src/app/fundamentos/14_flexbox/page.tsx
+++ react-e-next/react-fundamentos/src/app/fundamentos/14_flexbox/page.tsx
@@ -3,10 +3,11 @@ export default function Aplicacao() {
 
         // flex-row-reverse
         // order-2 grow justify-between items-start
-        <div className="border p-3 flex flex-row flex-wrap gap-3 justify-evenly items-start">
-            <div className="caixa h-28">Caixa 1</div>
-            <div className="caixa">Caixa 2</div>
-            <div className="caixa">Caixa 3</div>
+        // <div className="border p-3 flex flex-row flex-wrap gap-3 justify-evenly items-start">
+        <div className="border-4 p-3 flex gap-3">
+            <div className="caixa w-48 grow-0 shrink-0">Caixa 1</div>
+            <div className="caixa w-72 grow shrink">Caixa 2</div>
+            <div className="caixa w-96 grow-0 shrink-0">Caixa 3</div>
         </div>
     )
 }
