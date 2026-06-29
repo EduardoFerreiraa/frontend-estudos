22c7baefd7365ec06383f5f319b7e2f209896e6f
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-29 02:40:50 -0300

    Ordem, crescimento e alinhamento - Tailwind

--- react-e-next/react-fundamentos/src/app/fundamentos/14_flexbox/page.tsx
+++ react-e-next/react-fundamentos/src/app/fundamentos/14_flexbox/page.tsx
@@ -2,13 +2,11 @@ export default function Aplicacao() {
     return (
 
         // flex-row-reverse
-        <div className="border p-3 flex flex-row flex-wrap gap-3">
-            <div className="caixa basis-40">Caixa 1</div>
-            <div className="caixa basis-80">Caixa 2</div>
-            <div className="caixa basis-40">Caixa 3</div>
-            <div className="caixa basis-40">Caixa 4</div>
-            <div className="caixa basis-80">Caixa 5</div>
-            <div className="caixa basis-40">Caixa 6</div>
+        // order-2 grow justify-between items-start
+        <div className="border p-3 flex flex-row flex-wrap gap-3 justify-evenly items-start">
+            <div className="caixa h-28">Caixa 1</div>
+            <div className="caixa">Caixa 2</div>
+            <div className="caixa">Caixa 3</div>
         </div>
     )
 }
