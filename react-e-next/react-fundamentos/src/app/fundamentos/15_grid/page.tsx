eb26e8dcfb37630c1c0127e30751adebda57ae37
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-30 00:08:11 -0300

    Grid responsividade

--- react-e-next/react-fundamentos/src/app/fundamentos/15_grid/page.tsx
+++ react-e-next/react-fundamentos/src/app/fundamentos/15_grid/page.tsx
@@ -1,7 +1,8 @@
 export default function Aplicacao() {
     return (
-        <div className="border-2 p-3 grid grid-cols-4 gap-2">
-            <div className="caixa col-start-2">Caixa 1</div>
+        // <div className="border-2 p-3 grid grid-cols-4 gap-2">
+        <div className="border-2 p-3 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="caixa col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">Caixa 1</div>
             <div className="caixa">Caixa 2</div>
             <div className="caixa">Caixa 3</div>
             <div className="caixa">Caixa 4</div>
@@ -9,7 +10,7 @@ export default function Aplicacao() {
             <div className="caixa">Caixa 6</div>
             <div className="caixa">Caixa 7</div>
             <div className="caixa">Caixa 8</div>
-            <div className="caixa col-span-3">Caixa 9</div>
+            <div className="caixa">Caixa 9</div>
         </div>
     )
 }
