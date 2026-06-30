1dda943a0b830cf308d3ef134d980d25500c5ce4
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-30 02:22:06 -0300

    Utilizando a classe container no Tailwind

---
+++ react-e-next/react-fundamentos/src/app/fundamentos/16_container/page.tsx
@@ -0,0 +1,15 @@
+export default function Aplicacao() {
+    return (
+        <div className="container mx-auto bg-green-300 border-3">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia tenetur expedita voluptatum sapiente impedit! At fugit quod omnis placeat quos dolorem ipsam aliquid mollitia, voluptates totam fugiat, dolorum architecto blanditiis.</p>
+
+            <div className="border-3 border-white p-3 grid grid-cols-4 gap-2">
+                <div className="caixa col-span-1">Caixa 1</div>
+                <div className="caixa">Caixa 2</div>
+                <div className="caixa">Caixa 3</div>
+            </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nam ratione vel nostrum numquam architecto cupiditate perferendis et odit. Eligendi non nisi fuga sapiente ipsam, deserunt error recusandae animi obcaecati?</p>
+        </div>
+    )
+}
