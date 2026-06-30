508237ca1a98aa1795c9f74cccff5191a2f13e43
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-30 02:08:38 -0300

    Bordas em Tailwind

---
+++ next-e-tailwind/next-tailwind/src/app/6_bordas/page.tsx
@@ -0,0 +1,11 @@
+export default function Aplicacao() {
+    return (
+        <div>
+            <div className="m-8 p-3 border-2 rounded-3xl border-blue-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam laborum itaque rem vitae unde. Cupiditate, quam. Dicta pariatur vel possimus, ex consectetur quibusdam architecto est similique excepturi impedit commodi amet.
+            </div>
+
+            <button className="bg-blue-500 rounded-full px-6 py-2 m-5 cursor-pointer text-white hover:bg-blue-950 transition-all">Save changes</button>
+        </div>
+    )
+}
