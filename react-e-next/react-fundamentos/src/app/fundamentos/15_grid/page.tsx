57b2ea110bb1a1e9196928dec8b5c193abc50c01
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-29 23:32:07 -0300

    grid: colunas e espaçamentos

---
+++ react-e-next/react-fundamentos/src/app/fundamentos/15_grid/page.tsx
@@ -0,0 +1,15 @@
+export default function Aplicacao() {
+    return (
+        <div className="border-2 p-3 grid grid-cols-4 gap-2">
+            <div className="caixa col-start-2">Caixa 1</div>
+            <div className="caixa">Caixa 2</div>
+            <div className="caixa">Caixa 3</div>
+            <div className="caixa">Caixa 4</div>
+            <div className="caixa">Caixa 5</div>
+            <div className="caixa">Caixa 6</div>
+            <div className="caixa">Caixa 7</div>
+            <div className="caixa">Caixa 8</div>
+            <div className="caixa col-span-3">Caixa 9</div>
+        </div>
+    )
+}
