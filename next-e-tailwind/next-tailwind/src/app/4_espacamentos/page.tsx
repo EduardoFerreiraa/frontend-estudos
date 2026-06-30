b140347080d3f640161b5e763f16556998de6c89
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-29 23:51:52 -0300

    Padding e margin em Tailwind

---
+++ next-e-tailwind/next-tailwind/src/app/4_espacamentos/page.tsx
@@ -0,0 +1,22 @@
+export function Aplicacao2() {
+    return (
+        <div>
+            {/*
+                pt-6, py-5, px-5, p-[10px]
+            */}
+            <div className="bg-blue-300 p-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae quasi nostrum modi blanditiis cupiditate eius rerum esse reiciendis enim. Ad consequatur animi non quaerat necessitatibus expedita repellat architecto eligendi hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptate vitae, perspiciatis fugit delectus deleniti rerum non id sint iusto aut ipsa nostrum quas sapiente nisi quam, rem minima ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium voluptatibus laboriosam vero dignissimos consectetur eius ullam maxime. Tenetur, asperiores nobis. Commodi, nesciunt. Illum, perferendis. Fugiat deserunt aut enim magnam?</div>
+        </div>
+    )
+}
+
+export default function Aplicacao() {
+    return (
+        <div>
+            {/*
+                mr-8, ml-8, mt-8, mb-8, m-[10px], mx-5, my-10
+            */}
+            <div className="bg-blue-300 m-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae quasi nostrum modi blanditiis cupiditate eius rerum esse reiciendis enim. Ad consequatur animi non quaerat necessitatibus expedita repellat architecto eligendi hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptate vitae, perspiciatis fugit delectus deleniti rerum non id sint iusto aut ipsa nostrum quas sapiente nisi quam, rem minima ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium voluptatibus laboriosam vero dignissimos consectetur eius ullam maxime. Tenetur, asperiores nobis. Commodi, nesciunt. Illum, perferendis. Fugiat deserunt aut enim magnam?</div>
+            <h1 className="bg-red-500">Teste</h1>
+        </div>
+    )
+}
