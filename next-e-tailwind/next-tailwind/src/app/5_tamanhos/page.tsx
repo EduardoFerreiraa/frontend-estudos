0b9831557a0f14ddfab148d8ad9e415468884811
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-30 00:35:37 -0300

    Largura e altura em Tailwind

---
+++ next-e-tailwind/next-tailwind/src/app/5_tamanhos/page.tsx
@@ -0,0 +1,20 @@
+export function AplicacaoLargura() {
+    return (
+        <div className="flex">
+            <div className="
+                bg-orange-300 w-4/5
+            ">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt facere, nostrum 2
+            </div>
+        </div>
+    )
+}
+
+export default function Aplicacao() {
+    return (
+        <div className="bg-blue-300 h-screen">
+            <div className="bg-purple-500 h-1/2">Item 1</div>
+            <div className="bg-red-500 h-1/2">Item 2</div>
+        </div>
+    )
+}
