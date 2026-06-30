8691ac0a36bed7c68dc2deac15c53153bc8e6a09
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-30 00:51:43 -0300

    Grid alinhamentos em Tailwind'

--- react-e-next/react-fundamentos/src/app/fundamentos/15_grid/page.tsx
+++ react-e-next/react-fundamentos/src/app/fundamentos/15_grid/page.tsx
@@ -1,8 +1,11 @@
 export default function Aplicacao() {
     return (
         // <div className="border-2 p-3 grid grid-cols-4 gap-2">
-        <div className="border-2 p-3 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            <div className="caixa col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">Caixa 1</div>
+        // <div className="border-2 p-3 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        // <div className="caixa col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">Caixa 1</div>
+
+        <div className="border-2 p-3 h-screen grid grid-cols-4 gap-2 content-start">
+            <div className="caixa col-span-1">Caixa 1</div>
             <div className="caixa">Caixa 2</div>
             <div className="caixa">Caixa 3</div>
             <div className="caixa">Caixa 4</div>
